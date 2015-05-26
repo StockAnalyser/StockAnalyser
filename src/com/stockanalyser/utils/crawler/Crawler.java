2ef63ddf44b378c1510dec0b17362b4677b57ef3
Ian Wang <speng.wang@gmail.com>
2015-05-27 01:18:36 +0800

    Rejected commit: Default

--- src/com/stockanalyser/utils/crawler/Crawler.java
+++ src/com/stockanalyser/utils/crawler/Crawler.java
@@ -1,11 +1,34 @@
 package com.stockanalyser.utils.crawler;
 
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 /**
  * Created by wangshupeng on 27/5/15.
  */
 public class Crawler {
 
+    final String Url = "";
 
+    String StockNum;
+    String StartDate;
+    String EndDate;
+
+    public Crawler(String StockNum, String StartDate, String EndDate) {
+        this.StockNum = StockNum;
+        this.StartDate = StartDate;
+        this.EndDate = EndDate;
+    }
+
+    public void GetContent(){
+
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+
+        HttpGet httpGet = new HttpGet(this.Url);
+
+    }
 
 
 }
